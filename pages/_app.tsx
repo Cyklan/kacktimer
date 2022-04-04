25f7a6bacfb79dba472f513274d48a62f3e55577
Nils Fahle <nils.fahle@nitex.de>
2022-04-04 09:48:25 +0200

    new favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>Kacktimer</title>
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
 
-export default MyApp
+export default MyApp;
