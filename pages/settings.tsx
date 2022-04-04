9ffd6fc4c8803ea7c6d0e262725c6485debc68c6
Nils Fahle <nils.fahle@nitex.de>
2022-04-04 16:31:59 +0200

    fix logout button

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,16 +1,22 @@
 import { NextPage } from "next";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import BackButton from "../components/BackButton";
 
 const Settings: NextPage = () => {
+
+  const router = useRouter();
+
   return <div className="w-screen h-screen flex flex-col items-center justify-center">
     <BackButton to="/" />
     <div className="h-auto flex flex-col items-center justify-evenly space-y-8">
       <Link href="/setusername">
         <a className="btn btn-primary">Anzeigename ändern</a>
       </Link>
-      <button onClick={() => signOut()} className="btn btn-accent w-full">Abmelden</button>
+      <button onClick={() => {
+        signOut().then(() => router.push("/"))
+      }} className="btn btn-accent w-full">Abmelden</button>
     </div>
   </div>;
 };
