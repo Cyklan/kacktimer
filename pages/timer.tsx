f866a0a7b96e642cb60e332a6fc3fd4e94c42baa
Nils Fahle <nils.fahle@nitex.de>
2022-04-06 15:43:17 +0200

    replaced new Date() with Date.now(); replaced time update with calculation

--- pages/timer.tsx
+++ pages/timer.tsx
@@ -9,25 +9,25 @@ import dynamic from "next/dynamic";
 const Timer: NextPage = () => {
 
   const router = useRouter();
-  const [startTime, setStartTime] = useLocalStorage("startTime", new Date().getTime());
-  const [timePassed, setTimePassed] = useState(startTime === null ? 0 : Math.floor((new Date().getTime() - startTime) / 1000)); // in seconds
+  const [startTime, setStartTime] = useLocalStorage("startTime", Date.now());
+  const [timePassed, setTimePassed] = useState(startTime === null ? 0 : Math.floor((Date.now() - startTime) / 1000)); // in seconds
   const [endTime, setEndTime] = useLocalStorage<number>("endTime", null);
   const [showResultScreen, setShowResultScreen] = useLocalStorage<boolean>("showResultScreen", false);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTimePassed(t => t + 1);
+      setTimePassed(t => Math.floor((Date.now() - startTime) / 1000));
     }, 1000);
 
     return () => {
       clearInterval(interval);
     };
-  }, []);
+  }, [startTime]);
 
   if (showResultScreen && endTime) {
-    return <SavePoop 
-      startTime={startTime} 
-      endTime={endTime} 
+    return <SavePoop
+      startTime={startTime}
+      endTime={endTime}
       reset={() => {
         setStartTime(null);
         setEndTime(null);
@@ -48,7 +48,7 @@ const Timer: NextPage = () => {
         <label htmlFor="cancel-modal" className="btn btn-accent">Abbrechen</label>
         <button
           onClick={() => {
-            setEndTime(new Date().getTime());
+            setEndTime(Date.now());
             setShowResultScreen(true);
           }}
           className="btn btn-primary"
