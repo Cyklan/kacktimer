6f462402e483ae3a4dd4a49d870b17a2cd034fd1
Nils Fahle <nils.fahle@nitex.de>
2022-04-07 15:47:57 +0200

    fixed sync issue with poopstate

--- pages/api/sync.ts
+++ pages/api/sync.ts
@@ -39,7 +39,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       rating: x.rating,
       timeInMS: x.timeInMS,
       id: x.id,
-      withoutPoop: x.withPoop
+      withoutPoop: !x.withPoop
     }))
   });
 
