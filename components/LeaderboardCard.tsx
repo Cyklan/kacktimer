68f6c3ec6b861c9889afd3e3fac40432d0a14426
Nils Fahle <nils.fahle@nitex.de>
2022-04-19 11:19:30 +0200

    Leaderboard :YEP:

--- components/LeaderboardCard.tsx
+++ components/LeaderboardCard.tsx
@@ -4,14 +4,45 @@ interface LeaderboardProps {
   time: number;
   name: string;
   place: number;
+  highlight?: boolean;
 }
 
-const LeaderboardCard: FC<LeaderboardProps> = ({ name, place, time }) => {
-  return <div className="w-full mx-12 card">
-    <div className="card-body">
-      {name}
+const LeaderboardCard: FC<LeaderboardProps> = ({ name, place, time, highlight }) => {
+  
+  let color = "bg-orange-900";
+  switch (place) {
+    case 1:
+      color = "bg-yellow-400";
+      break;
+    case 2:
+      color = "bg-slate-300";
+      break;
+    case 3:
+      color = "bg-orange-600";
+      break;
+    default:
+      break;
+  }
+  
+  return <div className={`w-full mx-12 card ${highlight ? "shadow-2xl border-2 border-primary-focus " : "shadow-lg"}`}>
+    <div className="card-body p-2">
+      <div className="flex w-full h-full items-center">
+        <div className="avatar placeholder">
+          <div className={`${color} bg-oran text-white rounded-full w-10`}>
+            <span className="text-xl">{place === -1 ? ":(" : place}</span>
+          </div>
+        </div>
+        <span className="ml-2">{name}: {formatHoursToReadable(time)}</span>
+      </div>
     </div>
-  </div>
+  </div>;
+};
+
+const formatHoursToReadable = (timeInH: number) => {
+  const hours = Math.floor(timeInH);
+  const minutes = Math.floor((timeInH - hours) * 60);
+  const seconds = Math.floor(((timeInH - hours) * 60 - minutes) * 60);
+  return `${hours}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 }
 
 export default LeaderboardCard;
