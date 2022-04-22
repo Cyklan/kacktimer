ed525abc6896481b5909a69ca12832d92d99dd70
Nils Fahle <nils.fahle@nitex.de>
2022-04-22 12:07:56 +0200

    load app when offline

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,20 @@
 import { NextPage } from "next";
 import { useSession } from "next-auth/react";
+import { useContext } from "react";
 import Dashboard from "../components/Dashboard";
 import Hero from "../components/Hero";
+import { OnlineContext } from "./_app";
 
 const Home: NextPage = () => {
 
   const session = useSession();
+  const online = useContext(OnlineContext);
 
-  if (session.status === "unauthenticated") {
+  if (session.status === "unauthenticated" && (online || typeof window === "undefined")) {
     return <Hero />;
   }
 
-  if (session.status === "loading") {
+  if (session.status === "loading" && (online || typeof window === "undefined")) {
     return <></>;
   }
 
